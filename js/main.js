9ff9c097564b7b6015212680f0b8a88fca725ae9
Adam Husain <adamhusain12@gmail.com>
2022-08-07 12:18:05 +0800

    Improved product card design

--- js/main.js
+++ js/main.js
@@ -1,34 +1,83 @@
-window.onload = function(){
-    $("#loader-wrapper").fadeOut(700);
-    setTimeout(function(){
-        document.getElementById("fadein").remove();
-    },1000);
+const MaxImagesAllowed = 5
 
-    $('.material-card > .mc-btn-action').click(function () {
-        var card = $(this).parent('.material-card');
-        var icon = $(this).children('i');
-        icon.addClass('fa-spin-fast');
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById("fadein").remove()
+    },1000)
 
-        if (card.hasClass('mc-active')) {
-            card.removeClass('mc-active');
-
-            window.setTimeout(function() {
-                icon
-                    .removeClass('fa-arrow-left')
-                    .removeClass('fa-spin-fast')
-                    .addClass('fa-bars');
-
-            }, 800);
-        } else {
-            card.addClass('mc-active');
-
-            window.setTimeout(function() {
-                icon
-                    .removeClass('fa-bars')
-                    .removeClass('fa-spin-fast')
-                    .addClass('fa-arrow-left');
-
-            }, 800);
+    const product_titles = document.getElementsByClassName("product-title")
+    for (const title of product_titles) {
+        if (isElementOverflowing(title)) {
+            title.classList.add("marquee")
         }
-    });
+    }
 };
+
+/**
+ * Check if an element is overflowing
+ * @param element
+ * @returns {boolean}
+ */
+function isElementOverflowing(element) {
+    return element.scrollHeight > element.clientHeight
+        || element.scrollWidth > element.clientWidth
+}
+
+function carouselChange(cardIndex, increment) {
+    let currentImage = 0
+    let maxImages = 0
+    for (let i = 0; i < MaxImagesAllowed; i++) {
+        const image = document.getElementById("image" + cardIndex + "." + i)
+        if (image) {
+            maxImages = i
+            if (!image.classList.contains("hidden")) {
+                currentImage = i
+            }
+        }
+        else {
+            break
+        }
+    }
+
+    document.getElementById("image" + cardIndex + "." + currentImage).classList.add("hidden")
+    document.getElementById("indicator" + cardIndex + "." + currentImage).classList.remove("selected")
+    currentImage += increment
+    if (currentImage > maxImages) {
+        currentImage = 0
+    }
+    if (currentImage < 0) {
+        currentImage = maxImages
+    }
+
+
+    document.getElementById("image" + cardIndex + "." + currentImage).classList.remove("hidden")
+    document.getElementById("indicator" + cardIndex + "." + currentImage).classList.add("selected")
+}
+
+function like(cardIndex) {
+    document.getElementById("like" + cardIndex).classList.add("hidden")
+    document.getElementById("unlike" + cardIndex).classList.remove("hidden")
+}
+
+function unlike(cardIndex) {
+    document.getElementById("like" + cardIndex).classList.remove("hidden")
+    document.getElementById("unlike" + cardIndex).classList.add("hidden")
+}
+
+function toggleDesc(cardIndex) {
+    const product = getCard(cardIndex);
+    if (product.classList.contains("product-card-expanded")) {
+        product.classList.remove("product-card-expanded")
+    }
+    else {
+        product.classList.add("product-card-expanded")
+    }
+}
+
+function openLink(cardIndex) {
+
+}
+
+function getCard(cardIndex) {
+    return document.getElementsByClassName("product-card")[cardIndex]
+}
