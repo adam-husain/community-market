3ed74dab67125de117f226d2ad833af09800b467
Adam Husain <adamhusain12@gmail.com>
2022-08-10 21:04:07 +0800

    Testing PHP connections

--- smr/edit_profile.php
+++ smr/edit_profile.php
@@ -4,10 +4,16 @@ if (!hasPost('name') || !hasPost('pic') || !hasPost('pronouns')) {
     header('Location: edit_profile.html');
     exit();
 }
+
+$unique_id = uniqid (rand(), true);
 $name = $_POST['name'];
-$pic = $_POST['pic'];
 $pronouns = $_POST['pronouns'];
 
+
+$img_name = basename($_FILES['pic']['name']);
+$img_dir = 'img/profile/'.$img_name;
+
+echo($unique_id.':'.$img_dir.':'.$name.':'.$pronouns);
 
 function hasPost($varName): bool
 {
