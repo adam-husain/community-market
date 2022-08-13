9744b0e76e2a2a8c2c48d0057795a0cc886c6480
Adam Husain <adamhusain12@gmail.com>
2022-08-14 06:52:01 +0800

    Fixed block seller page without login

--- smr/js/seller.js
+++ smr/js/seller.js
@@ -1,18 +1,20 @@
+const oReq = new XMLHttpRequest()
+oReq.onload = function () {
+    const response = this.responseText
+    if (response === 'false') {
+        window.location.href = 'accounts.html';
+    } else {
+        const user = JSON.parse(response)
+        onLogin(user)
+    }
+};
+oReq.open("get",
+    "http://myresidence.shop/smr/server/checkSession.php", false)
+oReq.send()
+
 window.addEventListener('load', () => {
 
-    const oReq = new XMLHttpRequest()
-    oReq.onload = function () {
-        const response = this.responseText
-        if (response === 'false') {
-            window.location.href = 'accounts.html';
-        } else {
-            const user = JSON.parse(response)
-            onLogin(user)
-        }
-    };
-    oReq.open("get",
-        "http://myresidence.shop/smr/server/checkSession.php", false)
-    oReq.send()
+
 })
 
 function onLogin(user) {
