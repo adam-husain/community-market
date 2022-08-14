44820d3ecffe13142cc88e192c9f954e78f89288
Adam Husain <adamhusain12@gmail.com>
2022-08-14 19:40:43 +0800

    Fixed Profile page redirect

--- smr/js/profile.js
+++ smr/js/profile.js
@@ -4,4 +4,6 @@ function onLogin(user) {
     document.getElementById('name').value = user['name']
     document.getElementById('pronouns').value = parseInt(user['pronouns'])
     document.getElementById('whatsapp').value = user['whatsapp']
+
+    document.getElementById('profile-form').action = 'server/profile.php?session=' + session;
 }
