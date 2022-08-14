27fa37d410515744016013c3fbe4e88c386ed4c0
Adam Husain <adamhusain12@gmail.com>
2022-08-14 19:43:18 +0800

    Fixed Profile page redirect

--- smr/js/profile.js
+++ smr/js/profile.js
@@ -6,4 +6,22 @@ function onLogin(user) {
     document.getElementById('whatsapp').value = user['whatsapp']
 
     document.getElementById('profile-form').action = 'server/profile.php?session=' + session;
+
+    const urlParams = new URLSearchParams(window.location.search)
+    let code = urlParams.get('code')
+    if (code == null) return
+
+    switch (parseInt(code)) {
+        case 0:
+            showAlert('Profile Updated', 'Your profile has been updated')
+            break
+        case 1:
+            showAlert('Invalid Name', 'Please enter a proper name')
+            break
+        case 2:
+            showAlert('Invalid Whatsapp number', 'Please enter a valid Whatsapp number')
+            break
+        default:
+            showAlert('Database Error', 'Could not connect to database. Try again later')
+    }
 }
