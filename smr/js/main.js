be0e5a8d45b92f041bd10ab1c4bcb9af2ce0e868
Adam Husain <adamhusain12@gmail.com>
2022-08-14 17:24:22 +0800

    Fixing url encoded strings in main.js

--- smr/js/main.js
+++ smr/js/main.js
@@ -54,11 +54,11 @@ function displayUser(user) {
     const name = user['name']
     const pronouns = ['', '', 'He/Him', 'She/Her', 'They/Them'][user['pronouns']]
     const number = user['whatsapp'].replace('+', '')
-    const message = encodeURIComponent('Hello ' + name + '👋. I am interested in purchasing the product you have listed on the SMR Marketplace site.')
-    console.log(message)
-    const wa_url = 'http://wa.me/' + number + '?text=' + message
+    const message = 'Hello ' + name + '👋. I am interested in purchasing the product you have listed on the SMR Marketplace site.'
+    const wa_url = encodeURI('http://wa.me/' + number + '?text=' + message)
+    console.log(wa_url)
     const image_url = 'http://myresidence.shop/smr/img/profile/' + user['profile_url']
     frame.getElementById('title').innerHTML = name + '<small>' + pronouns + '</small>'
-    frame.getElementById('contact').innerHTML = 'Contact: <a href="' + encodeURIComponent(wa_url) + '"><i class="fa fa-whatsapp"> +' + number + '</i></a>'
+    frame.getElementById('contact').innerHTML = 'Contact: <a href="' + wa_url + '" target="_blank"><i class="fa fa-whatsapp"> +' + number + '</i></a>'
     frame.getElementById('profile-pic').src = image_url;
 }
