dfe676cd8739d7e6c43c46511a48e0e96919f3f9
Adam Husain <adamhusain12@gmail.com>
2022-08-23 23:36:08 +0800

    Smaller cards for mobile

--- smr/server/dbconnect.php
+++ smr/server/dbconnect.php
@@ -5,6 +5,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include 'imageCompress.php';
+
 $servername = "localhost";
 $username = "snyper";
 $password = "adamH123#";
