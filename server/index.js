e3a80d4a6b8d07ddd12526aadb298e868ae85b61
Adam <adamhusain12@gmail.com>
2022-11-28 21:26:17 +0800

    Fixed loading for any page

--- server/index.js
+++ server/index.js
@@ -67,9 +67,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.get('/:page', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 const server = https.createServer(credentials, app);
 server.listen(port, () => {
