e63bbecff16a8c03d6a95632a81f524fd3d7fb87
Adam <adamhusain12@gmail.com>
2022-12-05 10:40:17 +0800

    Updated build commands

--- commands.sh
+++ commands.sh
@@ -5,4 +5,4 @@ cd ./server/
 sudo forever start index.js
 cd ../client/
 sudo npm run build
-sudo serve -s build -l 443 --ssl-cert "cert/certificate.crt" --ssl-key "cert/private.key"
+sudo serve -s build -l 443 --ssl-cert "cert/certificate.crt" --ssl-key "cert/private.key" --no-port-switching
