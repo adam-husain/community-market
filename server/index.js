5bd3272eeee1b5a075e8e0e7982e17913f55607e
Adam <adamhusain12@gmail.com>
2022-12-05 10:35:02 +0800

    Disabled KeepAlive

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const app = express();
 const port = process.env.PORT || 443;
 
 app.use(cors());
+app.keepalive = false;
 app.use(bodyParser.json());         // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 	extended: true
