7807d85a18abde6c5e98aa1d7c29231b63c9d85c
Adam <adamhusain12@gmail.com>
2022-12-08 17:14:59 +0800

    Updated commands

--- commands.sh
+++ commands.sh
@@ -1,8 +1,10 @@
 sudo forever stopall
 git pull
 cd ./client/
+npm install
 sudo npm run build
 sudo rm -rf ../server/build
 sudo mv -f build ../server/
 cd ../server/
+npm install
 sudo forever start index.js
